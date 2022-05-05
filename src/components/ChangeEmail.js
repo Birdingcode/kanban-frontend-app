8f27ee8d5b92de7d283dd8f99597f822e021f412
Birdingcode <filbert.goh@gmail.com>
2022-05-05 08:48:22 +0800

    Add new files

---
+++ src/components/ChangeEmail.js
@@ -0,0 +1,212 @@
+import React, { useState, useEffect, useContext } from "react"
+import Page from "./Page"
+import DispatchContext from "../DispatchContext"
+import { useImmerReducer } from "use-immer"
+import Axios from "axios"
+import { CSSTransition } from "react-transition-group"
+
+function ChangeEmail() {
+  const appDispatch = useContext(DispatchContext)
+  const nodeRef = React.useRef(null)
+  const initialState = {
+    oldEmail: {
+      value: "",
+      hasErrors: false,
+      message: "",
+      isUnique: false,
+      checkCount: 0
+    },
+    newEmail: {
+      value: "",
+      hasErrors: false,
+      message: "",
+      isUnique: false,
+      checkCount: 0
+    },
+    cfmEmail: {
+      value: "",
+      hasErrors: false,
+      message: "",
+      checkCount: 0
+    },
+    submitCount: 0
+  }
+
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "emailImmediately":
+        draft.oldEmail.hasErrors = false
+        draft.oldEmail.value = action.value
+        return
+      case "emailAfterDelay":
+        if (!/^\S+@\S+$/.test(draft.oldEmail.value)) {
+          draft.oldEmail.hasErrors = true
+          draft.oldEmail.message = "You must provide a valid email address"
+        }
+        if (!draft.oldEmail.hasErrors && !action.noRequest) {
+          draft.oldEmail.checkCount++
+        }
+        return
+      case "emailUniqueResults":
+        if (action.value == true) {
+          draft.oldEmail.hasErrors = false
+          draft.oldEmail.isUnique = false
+        } else {
+          draft.oldEmail.isUnique = true
+          draft.oldEmail.hasErrors = true
+          draft.oldEmail.message = "Email not found in database."
+        }
+        return
+      case "newEmailImmediately":
+        draft.newEmail.hasErrors = false
+        draft.newEmail.value = action.value
+        return
+      case "newEmailAfterDelay":
+        if (!/^\S+@\S+$/.test(draft.newEmail.value)) {
+          draft.newEmail.hasErrors = true
+          draft.newEmail.message = "You must provide a valid Email address"
+        }
+        if (!draft.newEmail.hasErrors && !action.noRequest) {
+          draft.newEmail.checkCount++
+        }
+        return
+      case "newEmailUniqueResults":
+        if (action.value == true) {
+          draft.newEmail.hasErrors = true
+          draft.newEmail.isUnique = false
+          draft.newEmail.message = "Email already taken."
+        } else {
+          draft.newEmail.isUnique = true
+          draft.newEmail.hasErrors = false
+        }
+        return
+      case "emailMatch":
+        draft.cfmEmail.value = action.value
+        if (draft.cfmEmail.value != draft.newEmail.value) {
+          draft.cfmEmail.hasErrors = true
+          draft.cfmEmail.message = "Email does not match."
+        } else {
+          draft.cfmEmail.hasErrors = false
+        }
+        return
+      case "submitForm":
+        if (!draft.oldEmail.hasErrors && !draft.oldEmail.isUnique && !draft.newEmail.hasErrors && draft.newEmail.isUnique && !draft.cfmEmail.hasErrors) {
+          draft.submitCount++
+        }
+        return
+    }
+  }
+
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
+
+  useEffect(() => {
+    if (state.newEmail.value) {
+      const delay = setTimeout(() => dispatch({ type: "newEmailAfterDelay" }), 800)
+      return () => clearTimeout(delay)
+    }
+  }, [state.newEmail.value])
+
+  useEffect(() => {
+    if (state.oldEmail.value) {
+      const delay = setTimeout(() => dispatch({ type: "emailAfterDelay" }), 800)
+      return () => clearTimeout(delay)
+    }
+  }, [state.oldEmail.value])
+
+  useEffect(() => {
+    if (state.oldEmail.checkCount) {
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/doesEmailExist", { oldEmail: state.oldEmail.value })
+          dispatch({ type: "emailUniqueResults", value: response.data })
+        } catch (e) {
+          console.log("There was a problem or request canceled")
+        }
+      }
+      fetchResults()
+    }
+  }, [state.oldEmail.checkCount])
+
+  useEffect(() => {
+    if (state.newEmail.checkCount) {
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/doesNewEmailExist", { newEmail: state.newEmail.value })
+          dispatch({ type: "newEmailUniqueResults", value: response.data })
+        } catch (e) {
+          console.log("There was a problem or request canceled")
+        }
+      }
+      fetchResults()
+    }
+  }, [state.newEmail.checkCount])
+
+  useEffect(() => {
+    if (state.submitCount) {
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/changeEmail", { oldEmail: state.oldEmail.value, newEmail: state.newEmail.value, cfmEmail: state.cfmEmail.value }, { withCredentials: true })
+          appDispatch({ type: "login", data: response.data })
+        } catch (e) {
+          console.log(e.response)
+        }
+      }
+      fetchResults()
+      //return () => ourRequest.cancel()
+    }
+  }, [state.submitCount])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: "emailImmediately", value: state.oldEmail.value })
+    dispatch({ type: "emailAfterDelay", value: state.oldEmail.value, noRequest: true })
+    dispatch({ type: "newEmailImmediately", value: state.newEmail.value })
+    dispatch({ type: "newEmailAfterDelay", value: state.newEmail.value, noRequest: true })
+    dispatch({ type: "submitForm" })
+  }
+
+  return (
+    <Page title="Change Email">
+      <form onSubmit={handleSubmit}>
+        <div className="form">
+          <div className="form-body">
+            <div className="oldEmail">
+              <label className="form__label" htmlFor="oldEmail">
+                Old Email{" "}
+              </label>
+              <input onChange={e => dispatch({ type: "emailImmediately", value: e.target.value })} type="oldEmail" id="oldEmail" className="form__input" placeholder="Old Email" autoComplete="off" />
+              <CSSTransition nodeRef={nodeRef} in={state.oldEmail.hasErrors} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                <div className="alert alert-danger small liveValidateMessage">{state.oldEmail.message}</div>
+              </CSSTransition>
+            </div>
+            <div className="newEmail">
+              <label className="form__label" htmlFor="newEmail">
+                New Email{" "}
+              </label>
+              <input onChange={e => dispatch({ type: "newEmailImmediately", value: e.target.value })} className="form__input" type="newEmail" id="newEmail" placeholder="New Email" autoComplete="off" />
+              <CSSTransition nodeRef={nodeRef} in={state.newEmail.hasErrors} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                <div className="alert alert-danger small liveValidateMessage">{state.newEmail.message}</div>
+              </CSSTransition>
+            </div>
+            <div className="cfmEmail">
+              <label className="form__label" htmlFor="cfmEmail">
+                Confirm Email{" "}
+              </label>
+              <input onChange={e => dispatch({ type: "emailMatch", value: e.target.value })} className="form__input" type="cfmEmail" id="cfmEmail" placeholder="Confirm Email" autoComplete="off" />
+              <CSSTransition nodeRef={nodeRef} in={state.cfmEmail.hasErrors} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                <div className="alert alert-danger small liveValidateMessage">{state.cfmEmail.message}</div>
+              </CSSTransition>
+            </div>
+          </div>
+          <div className="footer">
+            <button type="submit" className="btn">
+              Change Email
+            </button>
+          </div>
+        </div>
+      </form>
+    </Page>
+  )
+}
+
+export default ChangeEmail
---
+++ src/components/ChangePassword.js
@@ -0,0 +1,219 @@
+import React, { useState, useEffect, useContext } from "react"
+import Page from "./Page"
+import DispatchContext from "../DispatchContext"
+import { useImmerReducer } from "use-immer"
+import Axios from "axios"
+import { CSSTransition } from "react-transition-group"
+
+function ChangePassword() {
+  const appDispatch = useContext(DispatchContext)
+  const nodeRef = React.useRef(null)
+  const initialState = {
+    username: {
+      value: "",
+      hasErrors: false,
+      message: "",
+      isUnique: false,
+      checkCount: 0
+    },
+    cfmpassword: {
+      value: "",
+      hasErrors: false,
+      message: "",
+      checkCount: 0
+    },
+    password: {
+      value: "",
+      hasErrors: false,
+      message: "",
+      checkCount: 0
+    },
+
+    submitCount: 0
+  }
+
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "usernameImmediately":
+        draft.username.hasErrors = false
+        draft.username.value = action.value
+        if (draft.username.value.length > 30) {
+          draft.username.hasErrors = true
+          draft.username.message = "Username cannot exceed 30 characters"
+        }
+        return
+      case "usernameAfterDelay":
+        if (draft.username.value.length < 3) {
+          draft.username.hasErrors = true
+          draft.username.message = "Username must be at least 3 characters."
+        }
+        if (!draft.username.hasErrors && !action.noRequest) {
+          draft.username.checkCount++
+        }
+        return
+      case "usernameUniqueResults":
+        //if sever send back value of true
+        if (action.value == false) {
+          draft.username.hasErrors = true
+          draft.username.isUnique = true
+
+          draft.username.message = "Username does not match database"
+        } else {
+          draft.username.isUnique = false
+          draft.username.hasErrors = false
+        }
+        return
+      case "passwordImmediately":
+        draft.password.hasErrors = false
+        draft.password.value = action.value
+        if (draft.password.value.length > 11) {
+          draft.password.hasErrors = true
+          draft.password.message = "Password cannot exceed 10 characters"
+        }
+        return
+      case "passwordAfterDelay":
+        if (draft.password.value.length < 7) {
+          draft.password.hasErrors = true
+          draft.password.message = "Password must be at least 8 characters"
+        }
+        if (!draft.password.hasErrors && !action.noRequest) {
+          draft.password.checkCount++
+        }
+        return
+      case "passwordChar":
+        if (action.value == false) {
+          draft.password.hasErrors = true
+          draft.password.message = "Password must contain alphabets, numbers and special Characters"
+        } else {
+        }
+        return
+      case "passwordMatch":
+        draft.cfmpassword.value = action.value
+        if (draft.cfmpassword.value != draft.password.value) {
+          draft.cfmpassword.hasErrors = true
+          draft.cfmpassword.message = "Password does not match."
+        } else {
+          draft.cfmpassword.hasErrors = false
+        }
+        return
+      case "submitForm":
+        if (!draft.username.hasErrors && !draft.username.isUnique && !draft.password.hasErrors && !draft.cfmpassword.hasErrors) {
+          draft.submitCount++
+        }
+        return
+    }
+  }
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
+
+  useEffect(() => {
+    if (state.username.value) {
+      const delay = setTimeout(() => dispatch({ type: "usernameAfterDelay" }), 800)
+      return () => clearTimeout(delay)
+    }
+  }, [state.username.value])
+
+  useEffect(() => {
+    if (state.password.value) {
+      const delay = setTimeout(() => dispatch({ type: "passwordAfterDelay" }), 800)
+      return () => clearTimeout(delay)
+    }
+  }, [state.password.value])
+
+  useEffect(() => {
+    if (state.username.checkCount) {
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/doesUsernameExist", { username: state.username.value })
+          dispatch({ type: "usernameUniqueResults", value: response.data })
+          console.log(response)
+        } catch (e) {
+          console.log("There was a problem or request canceled")
+        }
+      }
+      fetchResults()
+    }
+  }, [state.username.checkCount])
+
+  useEffect(() => {
+    if (state.password.checkCount) {
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/doesPasswordCondition", { password: state.password.value })
+          dispatch({ type: "passwordChar", value: response.data })
+        } catch (e) {
+          console.log("There was a problem or request canceled")
+        }
+      }
+      fetchResults()
+    }
+  }, [state.password.checkCount])
+
+  useEffect(() => {
+    if (state.submitCount) {
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/changePassword", { username: state.username.value, password: state.password.value, cfmpassword: state.cfmpassword.value }, { withCredentials: true })
+          appDispatch({ type: "login", data: response.data })
+        } catch (e) {
+          console.log(e.response)
+        }
+      }
+      fetchResults()
+      //return () => ourRequest.cancel()
+    }
+  }, [state.submitCount])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: "usernameImmediately", value: state.username.value })
+    dispatch({ type: "usernameAfterDelay", value: state.username.value, noRequest: true })
+    dispatch({ type: "passwordImmediately", value: state.password.value })
+    dispatch({ type: "passwordAfterDelay", value: state.password.value, noRequest: true })
+    dispatch({ type: "submitForm" })
+  }
+
+  return (
+    <Page title="Change Password">
+      <form onSubmit={handleSubmit}>
+        <div className="form">
+          <div className="form-body">
+            <div className="username">
+              <label className="form__label" htmlFor="username">
+                Username{" "}
+              </label>
+              <input onChange={e => dispatch({ type: "usernameImmediately", value: e.target.value })} className="form__input" type="text" id="firstName" placeholder="Username" autoComplete="off" />
+              <CSSTransition nodeRef={nodeRef} in={state.username.hasErrors} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                <div className="alert alert-danger small liveValidateMessage">{state.username.message}</div>
+              </CSSTransition>
+            </div>
+            <div className="password">
+              <label className="form__label" htmlFor="password">
+                New Password{" "}
+              </label>
+              <input onChange={e => dispatch({ type: "passwordImmediately", value: e.target.value })} className="form__input" type="password" id="password" placeholder="New Password" autoComplete="off" />
+              <CSSTransition nodeRef={nodeRef} in={state.password.hasErrors} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                <div className="alert alert-danger small liveValidateMessage">{state.password.message}</div>
+              </CSSTransition>
+            </div>
+            <div className="cfmpassword">
+              <label className="form__label" htmlFor="cfmpassword">
+                Confirm Password{" "}
+              </label>
+              <input onChange={e => dispatch({ type: "passwordMatch", value: e.target.value })} className="form__input" type="password" id="cfmpassword" placeholder="Confirm Password" autoComplete="off" />
+              <CSSTransition nodeRef={nodeRef} in={state.cfmpassword.hasErrors} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                <div className="alert alert-danger small liveValidateMessage">{state.cfmpassword.message}</div>
+              </CSSTransition>
+            </div>
+          </div>
+          <div className="footer">
+            <button type="submit" className="btn">
+              Change Password
+            </button>
+          </div>
+        </div>
+      </form>
+    </Page>
+  )
+}
+
+export default ChangePassword
